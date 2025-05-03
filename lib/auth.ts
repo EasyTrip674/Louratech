f0d4c272e91e66aee664cdbb1eedfc69f40d2de4
Sidy Mohamed Salim <sidymamadousalim@gmail.com>
2025-05-03 18:23:14 +0100

    start dev deployement

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,7 +31,6 @@ export const auth = betterAuth({
                     authorize:true,
                     member:true,
                     client:true,
-                   
                 }
             });
             return {
