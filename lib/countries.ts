11cb2952cf315cc69f2d15918f5b52a7e896d6fd
Sidy Mohamed Salim <sidymamadousalim@gmail.com>
2025-05-03 18:32:34 +0100

    update

--- lib/countries.ts
+++ lib/countries.ts
@@ -2,4 +2,25 @@ export const countriesCode = [
   { code: "GN", label: "+224" }, // Guinée
   { code: "FR", label: "+33" },  // France
   { code: "MA", label: "+212" }, // Maroc
+  { code: "SN", label: "+221" }, // Sénégal
+  { code: "BJ", label: "+229" }, // Bénin
+  { code: "CI", label: "+225" }, // Côte d'Ivoire
+  { code: "NE", label: "+227" }, // Niger
+  { code: "ML", label: "+223" }, // Mali
+  { code: "TG", label: "+228" }, // Togo
+  { code: "BF", label: "+226" }, // Burkina Faso
+  { code: "GM", label: "+220" }, // Gambie
+  { code: "SL", label: "+232" }, // Sierra Leone
+  { code: "LR", label: "+231" }, // Libéria
+  { code: "GH", label: "+233" }, // Ghana
+  { code: "NG", label: "+234" }, // Nigéria
+  { code: "CD", label: "+243" }, // République Démocratique du Congo
+  { code: "CG", label: "+242" }, // République du Congo
+  { code: "AO", label: "+244" }, // Angola
+  { code: "TZ", label: "+255" }, // Tanzanie
+  { code: "KE", label: "+254" }, // Kenya
+  { code: "UG", label: "+256" }, // Ouganda
+  { code: "RW", label: "+250" }, // Rwanda
+  { code: "ET", label: "+251" }, // Éthiopie
+  { code: "DJ", label: "+253" }, // Djibouti
 ];
