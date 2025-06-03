32692866ca1cb5655bf6a0bceb13f423a8766654
Sidy Mohamed Salim <sidymamadousalim@gmail.com>
2025-06-03 01:40:24 +0100

    update mail

--- lib/nodemailer/email.ts
+++ lib/nodemailer/email.ts
@@ -10,7 +10,12 @@ export const sendEmail = async ({
   html: string;
 }) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'mail.privateemail.com',
+    port: 465, // Utilisez 465 pour SSL ou 587 pour TLS/STARTTLS
+    tls: {
+      rejectUnauthorized: false // Utile si vous rencontrez des problèmes de certificat
+    },
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER, // ton email Gmail
       pass: process.env.GMAIL_APP_PASSWORD, // mot de passe d'application
