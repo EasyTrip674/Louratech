10a7713cdcb1c16d8ee325532888d7a3bb02929c
Sidy Mohamed Salim <sidymamadousalim@gmail.com>
2025-06-05 20:26:36 +0100

    udppate

--- context/CopilotProvider.tsx
+++ context/CopilotProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useCopilotActions } from "@/hooks/useCopilotActions";
+// import { useCopilotActions } from "@/hooks/useCopilotActions";
 
 export default function CopilotProvider({ children }: { children: React.ReactNode }) {
     // Initialiser les actions CopilotKit
-    useCopilotActions();
+    // useCopilotActions();
   
     
     return(
